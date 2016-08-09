583d059d1dff4fc7bbda48562a501e75252aca65
Nimil Peethambaran <nimil.p@hotmail.com>
2016-08-08 23:12:35 -0400

    add account page

--- www/templates/accounts/account.list.controller.js
+++ www/templates/accounts/account.list.controller.js
@@ -1,16 +1,22 @@
 (function () {
-    angular.module('account.module')
-    .controller('accountlistcontroller',['$scope','cardServices', function ($scope,$cardServices){
+  angular.module('account.module')
+  .controller('accountlistcontroller',['$scope','cardServices', function ($scope,$cardServices){
 
     success = function (response){
+
       $scope.cards= response.data.cards;
     }
     var error = function(error){
       console.log('Error while getting accounts '+error);
     }
 
-     $cardServices.getCards(success,error);
-  //  $scope.data = success();
-      //console.log($scope.data);
-    }]);
+    $cardServices.getCards(success,error);
+
+
+    $scope.addAccount = function(){
+      console.log('clicked it');
+    }
+  }]);
+
+
 })();
---
+++ www/templates/accounts/add/addaccount.js
@@ -0,0 +1,8 @@
+'use strict';
+(function(){
+  angular.module('account.module').controller('accountaddcontroller',['$scope',function($scope){
+    alert('');
+
+    console.log('in here');
+  }])
+})();
